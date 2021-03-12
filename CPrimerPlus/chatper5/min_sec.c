147aa44d750ea2d2e69e3f55154b197d39fb08d7
ZhuXingWuChang <2061007923@qq.com>
2021-03-12 14:56:54 +0800

    C Primer Plus 示例程序

--- CPrimerPlus/chatper5/min_sec.c
+++ CPrimerPlus/chatper5/min_sec.c
@@ -9,7 +9,7 @@ int main(void)
     int sec, min, left; // sec:秒数  min:分钟数  left:截断掉的分钟数
 
     printf("Convert seconds to minutes and seconds!\n");
-    printf("Enter the number of seconds (<= to quit):\n");
+    printf("Enter the number of seconds (<=0 to quit):\n");
     scanf("%d", &sec);
     while (sec > 0)
     {
@@ -17,7 +17,7 @@ int main(void)
         left = sec % SEC_PER_MIN; // 剩下的秒数
         printf("%d seconds is %d minutes, %d seconds.\n", sec,
                min, left);
-        printf("Enter next value (<= to quit):\n");
+        printf("Enter next value (<=0 to quit):\n");
         scanf("%d", &sec);
     }
     printf("Done!\n");

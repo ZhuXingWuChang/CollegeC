e33981d81aad56dcb808ede56bb1b3986351419f
ZhuXingWuChang <2061007923@qq.com>
2021-03-16 12:05:50 +0800

    C Primer Plus 编程练习

--- CPrimerPlus/chapter6/6_16_17.c
+++ CPrimerPlus/chapter6/6_16_17.c
@@ -6,17 +6,20 @@
  * 循环中执行操作e
  */
 #include <stdio.h>
-const double ANNUAL_INSTEREST_RATE = 0.08;   // 定义年利率为8%
-const double TEN_DOLLARS = 10.0;      // 取出的十万美元
+const double ANNUAL_INSTEREST_RATE = 0.08; // 定义年利率为8%
+const double TEN_DOLLARS = 10.0;           // 取出的十万美元
 int main(void)
 {
     double balance = 100.0; // 初始一百万美元
-    
-    while (balance < 0.0)
+    short year = 0;
+
+    while (balance > 0.0)
     {
-        balance*=
+        balance *= (1 + ANNUAL_INSTEREST_RATE);
+        balance -= TEN_DOLLARS;
+        year++;
     }
-    
+    printf("After %d years, you have no money.\n", year);
 
     return 0;
 }

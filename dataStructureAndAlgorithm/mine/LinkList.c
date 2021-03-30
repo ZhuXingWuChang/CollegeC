dafd0a6b76dee81c7be5c106d33f42153be50134
诸行无常 <2061007923@qq.com>
2021-03-31 00:16:53 +0800

    我的代码

--- dataStructureAndAlgorithm/mine/LinkList.c
+++ dataStructureAndAlgorithm/mine/LinkList.c
@@ -18,24 +18,86 @@
  * endADT
  */
 #include <stdio.h>
+#include <stdlib.h> // 提供了内存分配函数的原型
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+typedef int Status;
 typedef int ElemType;
 typedef struct Node
 {
     ElemType data;
-    struct Node *next; // 声明了一个指针变量next，next指向Node的地址
+    struct Node *next; // 声明了一个指针变量next,next指向Node的地址
 } Node;
-typedef struct Node *LinkList;  // 声明了一个指针变量类型LinkList，该类型存储Node类型的地址
-void InitList(LinkList); // 使用LinkList，即需要传入一个Node类型的地址
+typedef struct Node *LinkList; // Node是指针,Node的地址是二级指针,所以LinkList是一个二级指针类型
 
-void InitList(LinkList list)
+Status visit(ElemType elem);
+Status InitList(LinkList *linkList);
+Status ClearList(LinkList *linkList);
+int ListLength(LinkList linkList);
+Status GetElem(LinkList linkList, int i, ElemType *elem);
+int LocateElem(LinkList linkList, ElemType elem);
+Status ListInsert(LinkList *linkList, int i, ElemType elem);
+Status ListDelete(LinkList *linkList, int i, ElemType *elem);
+Status ListTraverse(LinkList linkList);
+void CreateListHead(LinkList *linklist, int elemNum);
+
+Status visit(ElemType elem)
 {
-    
-
-    return;
+    printf("%d ", elem);
+    return OK;
 }
+
+Status InitList(LinkList *linkList)
+{
+    // linklist = (LinkList *)malloc(sizeof(Node)); 这种写法是给一个二级指针分配了内存
+    *linkList=(LinkList)malloc(sizeof(Node));   // 给二级指针*linkList分配一个结点的内存
+    if (!(linkList))
+        return ERROR;
+    (*linkList)->next = NULL;
+    return OK;
+}
+
+Status ClearList(LinkList *linkList)
+{
+
+    if (ListLength(*linkList) == 0 && (*linkList) == NULL)
+        return ERROR;
+    LinkList currentNodeP = (*linkList)->next; // 令当前结点指针指向第一个结点
+    LinkList lastNodeP;
+    while (currentNodeP->next)
+    {
+        lastNodeP = currentNodeP;
+        currentNodeP = currentNodeP->next;
+        free(lastNodeP);
+    }
+    (*linkList) = currentNodeP;
+    return OK;
+}
+
+int ListLength(LinkList linkList)
+{
+    int count = 0;
+    LinkList currentNodeP = linkList->next;
+    while (currentNodeP)
+    {
+        count++;
+        currentNodeP = currentNodeP->next;
+    }
+    return count;
+}
+
+// Status ListTraverse(LinkList linkList)
+// {
+//     for ()
+// }
 
 int main(void)
 {
+    LinkList linkList;
+    InitList(&linkList);
+    printf("The length of the linkList is %d", ListLength(linkList));
+
     return 0;
 }
-

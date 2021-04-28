30c4d3c9b71a8ff53c722c04f6f246138c54500c
ZhuXingWuChang <2061007923@qq.com>
2021-04-28 22:35:23 +0800

    调试文件

---
+++ build/CMakeFiles/debug.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/debug.dir/Fibonacci.c.obj"
+  "debug.exe"
+  "debug.exe.manifest"
+  "debug.pdb"
+  "libdebug.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

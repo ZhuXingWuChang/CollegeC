30cd68945bbc6c0e239845d83d69b65cccc5288f
诸行无常 <2061007923@qq.com>
2021-05-19 22:53:56 +0800

    我的代码

--- dataStructureAndAlgorithm/mine/第5章串/String.c
+++ dataStructureAndAlgorithm/mine/第5章串/String.c
@@ -1,11 +1,7 @@
 /*
 ADT 串(string)
 Data
-<<<<<<< HEAD
-    串中元素仅由一个字符组成,相邻元素具有前驱和后继关系
-=======
     串中元素仅由一个字符组成,相邻元素句具有前驱和后继关系
->>>>>>> 1f27db7f3795500c90a2bf01f64bf816fcc90272
 Operation
     StrAssign(T,*chars):生成一个其值等于字符串常量chars的串T.
     StrCopy(T,S):串S存在,由串S复制得串T.
@@ -17,28 +13,16 @@ Operation
     SubString(Sub,S,pos,len):串S存在,1<=pos<=StrLength(S),
                             且0<=len<=StrLength(S)-pos+1,用Sub返
                             回串S的第pos个字符起长度为len的子串.
-<<<<<<< HEAD
-    Index(S,T,Pos):串S和T存在,T是非空串,1<=pos<=StrLength(S).
-                    若主串S中存在和串T值相同的字串,则返回它在著窜S中
-                    第pos个字符之后第一次出现的位置,否则返回0.
-    Replace(S,T,V):串S T V存在,T是飞控穿.用V替换主串S中出现的所有
-                    与T相等的不重叠的子串.
-    StrInsert(S,pos,T):串S存在,1<=pos<=StrLength(S).
-=======
     Index(S,T,pos):串S和T存在,T是非空串,1<=pos<=StrLength(S).
                     若主串S中存在和串T值相同的子串,则返回它在主串S中
                     第pos个字符之后第一次出现的位置,否则返回0.
     Replace(S,T,V):串S T V存在,T是非空串.用V替换主串S中出现的所有
                     与T相等的不重叠的子串.
     StrInsert(S,pos,T):串S和T存在,1<=pos<=StrLength(S).
->>>>>>> 1f27db7f3795500c90a2bf01f64bf816fcc90272
                         在串S的第pos个字符之前插入串T.
     StrDelete(S,pos,len):串S存在,1<=pos<=StrLength(S).
                         从串S中删除第pos个字符起长度为len的子串.
 endADT
-<<<<<<< HEAD
-*/
-=======
 */
 
 #include <string.h>
@@ -142,4 +126,3 @@ Status SubString(String subStr, String mainStr, int pos, int len)
 {
     return OK;
 }
->>>>>>> 1f27db7f3795500c90a2bf01f64bf816fcc90272

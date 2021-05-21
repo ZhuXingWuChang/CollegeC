34313d90a14dcc5401a6af92fd8396b2143c03da
ZhuXingWuChang <2061007923@qq.com>
2021-05-21 11:31:46 +0800

    我的代码

--- dataStructureAndAlgorithm/mine/第5章串/String.c
+++ dataStructureAndAlgorithm/mine/第5章串/String.c
@@ -16,12 +16,12 @@ Operation
     Index(S,T,pos):串S和T存在,T是非空串,1<=pos<=StrLength(S).
                     若主串S中存在和串T值相同的子串,则返回它在主串S中
                     第pos个字符之后第一次出现的位置,否则返回0.
-    Replace(S,T,V):串S T V存在,T是非空串.用V替换主串S中出现的所有
-                    与T相等的不重叠的子串.
     StrInsert(S,pos,T):串S和T存在,1<=pos<=StrLength(S).
                         在串S的第pos个字符之前插入串T.
     StrDelete(S,pos,len):串S存在,1<=pos<=StrLength(S).
                         从串S中删除第pos个字符起长度为len的子串.
+    Replace(S,T,V):串S T V存在,T是非空串.用V替换主串S中出现的所有
+                    与T相等的不重叠的子串.
 endADT
 */
 
@@ -121,8 +121,102 @@ Status Concat(String resultStr, String prevStr, String nextStr)
     return OK;
 }
 
-// 思路:
+// 思路:用一个for循环,将mainStr[pos]到mainStr[pos+len-1]赋值给subStr[1]到subStr[len]
+//      在循环开始之前,要检验pos和len的值是否合法
+//      在循环结束之后,要将len赋值给subStr[0]
 Status SubString(String subStr, String mainStr, int pos, int len)
 {
+    if (pos < 1 || pos > mainStr[0] || len < 0 || len + pos > mainStr[0] + 1)
+        return ERROR;
+    for (int i = 1; i <= len; i++)
+        subStr[i] = mainStr[pos + i - 1];
+    subStr[0] = len;
+    return OK;
+}
+
+// 思路:需要两个下标i和j,分别用他们来访问主串和子串
+//      进入循环(如果i和j都未超过数组的长度)
+//          如果mainStr[i]和subStr[j]相等
+//              i和j向前移动一位
+//          一旦mainStr[i]和subStr[j]不相等
+//              j回溯到1,并且i回溯到i-j+2也就是i和j开始比较的起始位置+1的位置
+//      循环end
+//      如果是因为j越界而结束循环
+//          找到了匹配串,返回OK
+//      否则,j没有越界而结束了循环
+//          没有找到匹配串,返回ERROR
+int Index(String subStr, String mainStr, int pos)
+{
+    int mainCursor = pos;
+    int subCursor = 1;
+    while (mainCursor <= mainStr[0] && subCursor < subStr[0])
+        if (mainStr[mainCursor] == subStr[subCursor])
+        {
+            mainCursor++;
+            subCursor++;
+        }
+        else
+        {
+            mainCursor = mainCursor - subCursor + 2;
+            subCursor = 1;
+        }
+    //mainStr:      abcd
+    //subStr:        bc
+    //pos:           2
+    //mainCursor:    234
+    //subCursor:     123(越界,退出循环)
+    //return:        2(2=4-2 即 return mainCursor-subStr[0])
+    if (subCursor > subStr[0])
+        return mainCursor - subStr[0];
+    else
+        return 0;
+}
+
+// 思路:需要一个strOfMain来存放mainStr中的子串
+//      进入循环(当mainCursor不越界时)
+//          取出以mainCursor为首的,长度和subStr一致的子串
+//          如果子串和subStr相等
+//              返回mainCursor
+//      循环end
+//      返回0
+int Index2(String subStr, String mainStr, int pos)
+{
+    String strOfMain;
+    int mainCursor = pos;
+    while (mainCursor - 1 + subStr[0] < mainStr[0])
+    {
+        SubString(strOfMain, mainStr, mainCursor, subStr[0]);
+        if (StrCompare(strOfMain, subStr))
+            return mainCursor;
+        mainCursor++;
+    }
+    return 0;
+}
+
+// 思路:首先判断pos是否越界
+//      然后分两种情况,一种是插入后超过了最大长度,另一种是插入后没有超过最大长度
+//      如果未超过最大长度
+//          首先从(主串末尾+插入串的长度)开始,将串的余下部分都向后移动insertStr[0]个位置
+//          然后从pos开始,将insertStr复制到这个空出来的空间里
+//      如果超过了最大长度
+//          首先从(主串末尾)开始,将串的余下部分都向后移动insertStr[0]个位置
+//          然后从pos开始,将insertStr复制到这个空出来的空间里
+Status StrInsert(String mainStr, int pos, String insertStr)
+{
+    if (pos < 1 || pos > MAXSIZE + 1)
+        return ERROR;
+    if (mainStr[0] + insertStr[0] <= MAXSIZE)
+    {
+        for (int i = mainStr[0]; i >= pos; i--)     // 以移动多少长度得到循环条件的关系
+            mainStr[i + insertStr[0]] = mainStr[i]; // 以移动的间隔得到赋值条件的关系
+        for (int i = 1; i <= insertStr[0]; i++)     // 以插入多少长度得到循环条件的关系
+            mainStr[i - 1 + pos] = insertStr[i];
+    }
+    else
+    {
+        for (int i = MAXSIZE + 1 - insertStr[0]; i >= pos; i--) // 以移动多少长度得到循环关系
+            mainStr[i + insertStr[0]] = mainStr[i];             // 以移动的间隔得到赋值关系
+        for (int i = )
+    }
     return OK;
 }
